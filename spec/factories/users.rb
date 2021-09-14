8cf3e7015527d0b813a66ad409ec13bcba2ade4e
minaroka <sheep.mb0401@gmail.com>
2021-09-14 16:42:36 +0900

    FactoryBot,Fakerインストール準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    name                  {'test'}
+    email                 {'test@example'}
+    password              {'000000'}
+    password_confirmation {password}
+    profile               {}
+    occupation            {}
+    position              {}
+  end
+end
