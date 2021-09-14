e62d194e253902aa665da2c37e20f6fc31cf5f50
minaroka <sheep.mb0401@gmail.com>
2021-09-14 17:18:26 +0900

    Faker記入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name                  {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    name                  {Faker::Name.last_name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    profile               {}
-    occupation            {}
-    position              {}
+    profile               {Faker::Lorem.sentence}
+    occupation            {Faker::Lorem.sentence}
+    position              {Faker::Lorem.sentence}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
-    it ''
+    context '新規登録できるとき' do
+      it 'email、encrypted_password、password_confirmation、name、profile、occupation、positionが存在すれば登録できる'
+        expect(@user).to be_valid
+      end
+      it 'passwordとpassword_confirmationが６文字以上なら登録できる'
+        @user.password = '000000'
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do  
+      it 'emailが空だと登録できない'
+        
+      end
+      it 'emailが重複していると登録できない'
+      end
+      it 'encrypted_passwordが空だと登録できない'
+      end
+      it 'encrypted_passwordは5文字以下だと登録できない'
+      end
+      it 'password_confirmationが空だと登録できない'
+      end
+      it 'encrypted_passwordとpassword_confirmationが一致していないと登録できない'
+      end
+      it 'nameが空だと登録できない'
+      end
+      it 'profileが空だと登録できない'
+      end
+      it 'occupationが空だと登録できない'
+      end
+      it 'positionが空だと登録できない'
+      end
+    end
   end
 end
