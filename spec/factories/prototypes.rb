e6b0869bbef1228b85577552e12b87a053890979
takayun <beachshot10@gmail.com>
2021-09-15 17:30:14 +0900

    prototypeモデルの単体テストコード

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :prototype do
-    
+    title {Faker::Lorem.sentence}
+    catch_copy {Faker::Lorem.sentence}
+    concept {Faker::Lorem.sentence}
+    image {Faker::Lorem.sentence}
+    association :user 
+
+    after(:build) do |prototype|
+      prototype.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Prototype, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @prototype = FactoryBot.build(:prototype)
+  end
+
+  describe 'プロトタイプの保存' do
+    context 'プロトタイプが投稿できる場合' do
+      it 'titleとcatch_copyとconceptとimageが存在していれば保存できる' do
+        expect(@prototype).to be_valid
+      end
+    end
+    context 'プロトタイプが投稿できない場合' do
+      it 'titleが空では投稿できない' do
+        @prototype.title = ''
+        @prototype.valid?
+        expect(@prototype.errors.full_messages).to include("Title can't be blank")
+      end     
+      it 'catch_copyが空では投稿できない' do
+        @prototype.catch_copy = ''
+        @prototype.valid?
+        expect(@prototype.errors.full_messages).to include("Catch copy can't be blank")
+      end     
+      it 'conceptが空では投稿できない' do
+        @prototype.concept = ''
+        @prototype.valid?
+        expect(@prototype.errors.full_messages).to include("Concept can't be blank")
+      end     
+      it 'imageが空では投稿できない' do
+        @prototype.image = nil
+        @prototype.valid?
+        expect(@prototype.errors.full_messages).to include("Image can't be blank")
+      end     
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @prototype.user = nil
+        @prototype.valid?
+        expect(@prototype.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
