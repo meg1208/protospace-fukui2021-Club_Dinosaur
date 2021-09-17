3cc3a967b205007a90313933fbcf6723da5d0433
banan2021 <nishioka_odk@yahoo.co.jp>
2021-09-18 08:22:57 +0900

    コメント表示欄を投稿詳細に追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -17,12 +19,17 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
