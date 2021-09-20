06a3e9843f35c5d5e0480112c3d01de07f2d9664
meg1208 <meg8130@gmail.com>
2021-09-20 17:05:11 +0900

    createアクション前にauthenticate_user!を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  
   def create
     comment = Comment.create(comment_params)
     redirect_to "/prototypes/#{comment.prototype.id}"
