78674381a24a704487d424114bdc0f5a9c8c293e
takayun <beachshot10@gmail.com>
2021-09-22 16:49:33 +0900

    ウィザード新規登録コントローラーに、new,createアクションを定義

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,22 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(sign_up_params)
+     unless @user.valid?
+       render :new and return
+     end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+  
   # POST /resource
   # def create
   #   super
