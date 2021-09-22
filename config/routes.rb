e50fd7a2dd8b0a0171052093174cf141e47fd039
takayun <beachshot10@gmail.com>
2021-09-22 16:15:04 +0900

    新規登録関係のルーティングをdevise管理下のコントローラーからuserディレクトリのregistrationsコントローラーに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: 'prototypes#index'
   resources :prototypes do
     resources :comments, only: [:create]
