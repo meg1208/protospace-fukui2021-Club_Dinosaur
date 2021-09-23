7a7b632074c4c70edbaed574982e4f91143a7ad3
banan2021 <nishioka_odk@yahoo.co.jp>
2021-09-23 18:54:02 +0900

    全角スペース削除

--- db/migrate/20210922054542_create_addresses.rb
+++ db/migrate/20210922054542_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.integer :postal_code,   null: false
-      t.text :address,          null: false　
+      t.text :address,          null: false
       t.references :user
       t.timestamps
     end
