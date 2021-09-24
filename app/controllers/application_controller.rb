170938582d4e08b407b1f2d47e671d260038ea7e
banan2021 <nishioka_odk@yahoo.co.jp>
2021-09-25 07:32:20 +0900

    Basic認証再表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
 
   # def basic_auth
     # if Rails.env.production?
-      # authenticate_or_request_with_http_basic do |username, password|
-        # username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
-      # end
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+      end
     # end
   # end
 # end
