5b87f2f65d8c76b457fffdd7bf3bf2e473a4fc4f
meg1208 <meg8130@gmail.com>
2021-09-24 11:25:37 +0900

    Revert "編集画面表示時に、すでに登録されている画像を表示させる機能"
    This reverts commit 5b95fae5e5e74f52c2e3f8347e2cec80458f2a07.

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -7,28 +7,27 @@ function previewImage() {
     const imagePreview = document.getElementById('prototype_image_preview')
 
     // プロトタイプの画像変更時の処理
-    prototype_image.addEventListener('change', function(e){
-      
-      // プロトタイプの画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.getElementById("image_content");
+    document.getElementById('prototype_image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
       if (imageContent) {
         imageContent.remove();
       }
       
-      // 画像表示領域の要素を取得
-      const image_section = document.getElementById('image_section')
-      // ファイル情報を取得
       const file = e.target.files[0]
-      // ファイルのURLを作成
       const blob = window.URL.createObjectURL(file)
+
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
 
       // 表示する画像を生成
       const blobImage = document.createElement('img')
-      blobImage.setAttribute('id', 'image_content')
       blobImage.setAttribute('src', blob)
 
       // 生成したHTMLの要素をブラウザに表示させる
-      image_section.appendChild(blobImage)
+      imageElement.appendChild(blobImage)
+      imagePreview.appendChild(imageElement)
     })
   }
 }
+
